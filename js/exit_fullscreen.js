f4fe7206b4cd91fdbd730b367cd626001e790506
Dominik Hebeler <dominik@hebeler.club>
2022-05-17 22:22:53 +0200

    removed console log

--- js/exit_fullscreen.js
+++ js/exit_fullscreen.js
@@ -4,7 +4,6 @@ videos.forEach(video => {
     video.addEventListener("ended", (e) => {
         // Find out if autoplay is active
         let autoplay_active = document.querySelector(".ytp-autonav-toggle-button").getAttribute("aria-checked")
-        console.log(autoplay_active);
         if (autoplay_active !== 'true') {
             document.exitFullscreen();
         }
