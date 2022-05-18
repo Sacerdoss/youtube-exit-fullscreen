ab9e0c8bf2ed6ed466c11a9e15dcd5e13eccb279
Dominik Hebeler <dominik@suma-ev.de>
2022-05-18 16:40:44 +0200

    catching all video end events

--- js/exit_fullscreen.js
+++ js/exit_fullscreen.js
@@ -1,11 +1,55 @@
-let videos = document.querySelectorAll("video");
+video = document.querySelector("video");
+addEvents(video);
 
-videos.forEach(video => {
-    video.addEventListener("ended", (e) => {
-        // Find out if autoplay is active
-        let autoplay_active = document.querySelector(".ytp-autonav-toggle-button").getAttribute("aria-checked")
-        if (autoplay_active !== 'true') {
+document.addEventListener("DOMContentLoaded", event => {
+    video = document.querySelector("video");
+    addEvents(video);
+})
+
+document.addEventListener("DOMNodeInserted", event => {
+    if (event.target.nodeName === "VIDEO") {
+        addEvents(event.target);
+    }
+});
+
+function addEvents(node) {
+    node.addEventListener("ended", end_fullscreen);
+    // The ended event is not always thrown by youtube. We will check on pause event if the video ended
+    node.addEventListener("pause", paused_video);
+}
+
+
+
+function end_fullscreen() {
+    // Find out if autoplay is active
+    let autoplay_active = false;
+
+    let desktop_container = document.querySelector(".ytp-autonav-toggle-button");
+    if (desktop_container !== null && desktop_container.hasAttribute("aria-checked") && desktop_container.getAttribute("aria-checked") === 'true') {
+        autoplay_active = true;
+    }
+
+    let mobile_container = document.querySelector(".ytm-autonav-toggle-button-container");
+    if (mobile_container != null && mobile_container.hasAttribute("aria-pressed") && mobile_container.getAttribute("aria-pressed") === 'true') {
+        autoplay_active = true;
+    }
+
+    if (autoplay_active === false) {
+        if (window.fullScreen) {
             document.exitFullscreen();
         }
-    });
-})
+    }
+}
+
+function paused_video() {
+    video = document.querySelector("video");
+    let duration = video.duration;
+    let currentTime = video.currentTime;
+    /** 
+     * If playback stopped with less than a second of the video left: exit fullscreen 
+     * If the end is reached through seeking the video position is not exactly at the end
+     * */
+    if (duration - currentTime < 10) {
+        end_fullscreen();
+    }
+}
